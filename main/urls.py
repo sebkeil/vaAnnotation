e1f893dcba494a1511d909a5588a85ddab284c00
Keil <sebastian.keil@accenture.com>
2022-05-12 15:48:00 +0200

    initial commit

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("annotate", views.annotate_view, name='annotate_view'),
+    path("annotate/<int:id>", views.sentence_view, name='sentence_view'),
+    path("guideline", views.guideline_view, name='guideline_view')
+
+]
+
